76d6f8838c9d2e2d3e8cbaa976749fb77256db50
Lucas <silvalucas1912@gmail.com>
2023-01-26 14:26:36 -0300

    implementation backend aula

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,5 +1,5 @@
 import "reflect-metadata";
-import "../../database";
+import "../../database/migrations";
 import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import routes from "./routes";
